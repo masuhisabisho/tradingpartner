bd1869c76ad28e689ff36fef21f19d37203a5d01
masuhisabisho <madman190382@yahoo.co.jp>
2013-06-09 16:30:32 +0900

    commit4

--- tradingpartner/MainForm.Designer.vb
+++ tradingpartner/MainForm.Designer.vb
@@ -33,89 +33,90 @@ Partial Class MainForm
 	''' not be able to load this method if it was changed manually.
 	''' </summary>
 	Private Sub InitializeComponent()
-		Me.textBox1 = New System.Windows.Forms.TextBox()
-		Me.button1 = New System.Windows.Forms.Button()
-		Me.label1 = New System.Windows.Forms.Label()
-		Me.label2 = New System.Windows.Forms.Label()
-		Me.label3 = New System.Windows.Forms.Label()
-		Me.label4 = New System.Windows.Forms.Label()
+		Me.Txt_Password = New System.Windows.Forms.TextBox()
+		Me.Brn_Login = New System.Windows.Forms.Button()
+		Me.Lbl_Ver = New System.Windows.Forms.Label()
+		Me.Lbl_VerNo = New System.Windows.Forms.Label()
+		Me.Lbl_Update = New System.Windows.Forms.Label()
+		Me.Lbl_Date = New System.Windows.Forms.Label()
 		Me.SuspendLayout
 		'
-		'textBox1
+		'Txt_Password
 		'
-		Me.textBox1.Font = New System.Drawing.Font("MS UI Gothic", 14!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128,Byte))
-		Me.textBox1.Location = New System.Drawing.Point(188, 100)
-		Me.textBox1.Name = "textBox1"
-		Me.textBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-		Me.textBox1.Size = New System.Drawing.Size(162, 26)
-		Me.textBox1.TabIndex = 0
+		Me.Txt_Password.Font = New System.Drawing.Font("MS UI Gothic", 14!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128,Byte))
+		Me.Txt_Password.Location = New System.Drawing.Point(165, 83)
+		Me.Txt_Password.Name = "Txt_Password"
+		Me.Txt_Password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+		Me.Txt_Password.Size = New System.Drawing.Size(162, 26)
+		Me.Txt_Password.TabIndex = 0
 		'
-		'button1
+		'Brn_Login
 		'
-		Me.button1.Location = New System.Drawing.Point(378, 99)
-		Me.button1.Name = "button1"
-		Me.button1.Size = New System.Drawing.Size(75, 27)
-		Me.button1.TabIndex = 1
-		Me.button1.Text = "button1"
-		Me.button1.UseVisualStyleBackColor = true
+		Me.Brn_Login.Location = New System.Drawing.Point(355, 82)
+		Me.Brn_Login.Name = "Brn_Login"
+		Me.Brn_Login.Size = New System.Drawing.Size(75, 27)
+		Me.Brn_Login.TabIndex = 1
+		Me.Brn_Login.Text = "ログイン"
+		Me.Brn_Login.UseVisualStyleBackColor = true
 		'
-		'label1
+		'Lbl_Ver
 		'
-		Me.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-		Me.label1.Location = New System.Drawing.Point(50, 213)
-		Me.label1.Name = "label1"
-		Me.label1.Size = New System.Drawing.Size(100, 23)
-		Me.label1.TabIndex = 2
-		Me.label1.Text = "label1"
+		Me.Lbl_Ver.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+		Me.Lbl_Ver.Location = New System.Drawing.Point(168, 196)
+		Me.Lbl_Ver.Name = "Lbl_Ver"
+		Me.Lbl_Ver.Size = New System.Drawing.Size(50, 14)
+		Me.Lbl_Ver.TabIndex = 2
+		Me.Lbl_Ver.Text = "バージョン"
 		'
-		'label2
+		'Lbl_VerNo
 		'
-		Me.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-		Me.label2.Location = New System.Drawing.Point(169, 213)
-		Me.label2.Name = "label2"
-		Me.label2.Size = New System.Drawing.Size(100, 23)
-		Me.label2.TabIndex = 3
-		Me.label2.Text = "label2"
+		Me.Lbl_VerNo.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+		Me.Lbl_VerNo.Location = New System.Drawing.Point(224, 196)
+		Me.Lbl_VerNo.Name = "Lbl_VerNo"
+		Me.Lbl_VerNo.Size = New System.Drawing.Size(38, 14)
+		Me.Lbl_VerNo.TabIndex = 3
+		Me.Lbl_VerNo.Text = "0.0.0.0"
 		'
-		'label3
+		'Lbl_Update
 		'
-		Me.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-		Me.label3.Location = New System.Drawing.Point(298, 213)
-		Me.label3.Name = "label3"
-		Me.label3.Size = New System.Drawing.Size(100, 23)
-		Me.label3.TabIndex = 4
-		Me.label3.Text = "label3"
+		Me.Lbl_Update.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+		Me.Lbl_Update.Location = New System.Drawing.Point(316, 196)
+		Me.Lbl_Update.Name = "Lbl_Update"
+		Me.Lbl_Update.Size = New System.Drawing.Size(88, 14)
+		Me.Lbl_Update.TabIndex = 4
+		Me.Lbl_Update.Text = "最終更新日時:"
 		'
-		'label4
+		'Lbl_Date
 		'
-		Me.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-		Me.label4.Location = New System.Drawing.Point(423, 213)
-		Me.label4.Name = "label4"
-		Me.label4.Size = New System.Drawing.Size(100, 23)
-		Me.label4.TabIndex = 5
-		Me.label4.Text = "label4"
+		Me.Lbl_Date.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+		Me.Lbl_Date.Location = New System.Drawing.Point(409, 196)
+		Me.Lbl_Date.Name = "Lbl_Date"
+		Me.Lbl_Date.Size = New System.Drawing.Size(100, 14)
+		Me.Lbl_Date.TabIndex = 5
+		Me.Lbl_Date.Text = "0000/00/00 00:00"
 		'
 		'MainForm
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 12!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(552, 285)
-		Me.Controls.Add(Me.label4)
-		Me.Controls.Add(Me.label3)
-		Me.Controls.Add(Me.label2)
-		Me.Controls.Add(Me.label1)
-		Me.Controls.Add(Me.button1)
-		Me.Controls.Add(Me.textBox1)
+		Me.BackColor = System.Drawing.Color.White
+		Me.ClientSize = New System.Drawing.Size(516, 222)
+		Me.Controls.Add(Me.Lbl_Date)
+		Me.Controls.Add(Me.Lbl_Update)
+		Me.Controls.Add(Me.Lbl_VerNo)
+		Me.Controls.Add(Me.Lbl_Ver)
+		Me.Controls.Add(Me.Brn_Login)
+		Me.Controls.Add(Me.Txt_Password)
 		Me.Name = "MainForm"
 		Me.Text = "ログイン"
 		AddHandler Load, AddressOf Me.LoginLoad
 		Me.ResumeLayout(false)
 		Me.PerformLayout
 	End Sub
-	Private label4 As System.Windows.Forms.Label
-	Private label3 As System.Windows.Forms.Label
-	Private label2 As System.Windows.Forms.Label
-	Private label1 As System.Windows.Forms.Label
-	Private button1 As System.Windows.Forms.Button
-	Private textBox1 As System.Windows.Forms.TextBox
+	Private Txt_Password As System.Windows.Forms.TextBox
+	Private Brn_Login As System.Windows.Forms.Button
+	Private Lbl_Ver As System.Windows.Forms.Label
+	Private Lbl_VerNo As System.Windows.Forms.Label
+	Private Lbl_Update As System.Windows.Forms.Label
+	Private Lbl_Date As System.Windows.Forms.Label
 End Class
